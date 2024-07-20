dfc2963b7b43b2528be8f419c04e2fc9c788fe30
Bhavy_Zala <zalabhavy2004@gmail.com>
2024-07-21 00:25:18 +0530

    Contributors Page added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import { Route, Routes } from "react-router-dom";
-import Navbar from "./Component/Navbar";
-import Home from "./Component/Home";
-import Doc from "./Component/Documetation/Doc";
-import Error from "./Component/Error";
-import InternPage from "./Component/Documetation/Internship/InternPage";
-import Footer from "./Component/Footer";
-import Contact from "./Component/Contact";
-import Blog from "./Component/Documetation/Blog";
-import { useState } from "react";
-
+import React, { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './Component/Navbar';
+import Home from './Component/Home';
+import Doc from './Component/Documetation/Doc';
+import Error from './Component/Error';
+import InternPage from './Component/Documetation/Internship/InternPage';
+import Footer from './Component/Footer';
+import Contact from './Component/Contact';
+import Blog from './Component/Documetation/Blog';
+import Contributors from './Component/Contributors'; 
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -19,20 +19,22 @@ function App() {
       setLoading(false);
     }, 2000);
   }
-  return(
+  return (
     !loading && (
- <div>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/docs" element={<Doc/>}/>
-        <Route path="/intern" element={<InternPage/>}/>
-        <Route path="/blog" element={<Blog />} />
-        <Route path="/Contact" element={<Contact></Contact>}/>
-        <Route path="*" element={<Error/>}/>
-      </Routes>
-      <Footer/> 
-    </div>)
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/docs" element={<Doc />} />
+          <Route path="/intern" element={<InternPage />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/contributors" element={<Contributors />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer />
+      </div>
+    )
   );
 }
 
